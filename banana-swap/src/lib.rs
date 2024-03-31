599ccc48e00c3d8a07b579c02e6a89186cd57c3f
gwertem <155088988+gwertem@users.noreply.github.com>
2024-03-31 13:04:26 +0200

    wip: Codebase for stackoverflow

--- banana-swap/src/lib.rs
+++ banana-swap/src/lib.rs
@@ -1,45 +1,11 @@
-use borsh::{BorshSerialize, BorshDeserialize};
-
-// this is causing problem, even if nothing is used
-// use anchor_lang::prelude::{borsh::{BorshDeserialize, BorshSerialize, try_from_slice_with_schema}, *};
-
-// Host functions
-extern "C" {
-    fn print_number_to_console(num: i32);
-}
-
-// use anchor_lang::solana_program::pubkey::Pubkey;
-
-#[derive(Debug, BorshSerialize, BorshDeserialize)]
-pub struct TxData {
-    pub slot: u64,
-    pub account_keys: Vec<[u8; 32]>,
-    pub logs: Vec<String>,
-}
-
-// #[no_mangle]
-// pub extern "C" fn add(a: i32, b: i32) -> i32 {
-//     let result = a + b;
-
-//     unsafe {
-//         print_number_to_console(result)
-//     };
-
-//     result
-// }
-
 #[no_mangle]
-pub extern "C" fn process(ptr: *const u8, len: usize) -> i32 {
-    // println!("in process()");
-    let bytes = unsafe { std::slice::from_raw_parts(ptr, len) };
+pub extern "C" fn process_bytes(ptr: *const u8, len: usize) -> u8 {
+    if len > 0 {
+        let bytes = unsafe { std::slice::from_raw_parts(ptr, len) };
+        let first = bytes[0];
 
-    let first = bytes[0];
-
-    first as i32
-    // len as i32
-    // // println!("Received data: {:?}", data_slice);
-
-    // let decoded_data = TxData::try_from_slice(data_slice).unwrap();
-
-    // println!("Decoded {:?}", decoded_data);
+        first
+    } else {
+        0
+    }
 }
--- indexer-core/src/main.rs
+++ indexer-core/src/main.rs
@@ -1,11 +1,6 @@
 pub mod get_tx_data;
 
-use anchor_lang::{prelude::borsh::to_vec, AnchorDeserialize, AnchorSerialize};
-use wasmer::{imports, Cranelift, Engine, Function, Instance, Memory, MemoryType, Module, NativeEngineExt, Store, Value};
-use std::io::{self, Write};
-use solana_client;
-
-use crate::get_tx_data::{get_tx_data, TxData};
+use wasmer::{imports, Cranelift, Instance, Module, Store, Value};
 
 fn main() {
     // Read the Wasm file
@@ -14,56 +9,28 @@ fn main() {
     // Create a store
     let mut store = Store::default();
 
-    // Memory
-    let memory = Memory::new(&mut store, MemoryType::new(1, None, false)).unwrap();
+    // let compiler = Cranelift::default();
+    // let mut store = Store::new(compiler);
 
     // Compile the module
     let module = Module::new(&store, wasm_bytes).unwrap();
 
     // Create an import object with the host function
-    let import_object = imports! {
-        "env" => {
-            // Pass the host function as an import
-            "print_number_to_console" => Function::new_typed(&mut store, print_number_to_console),
-            "memory" => memory,
-        },
-    };
+    let import_object = imports! {};
 
     let instance = Instance::new(&mut store, &module, &import_object).unwrap();
-    let function = instance.exports.get_function("process").unwrap();
+    let function = instance.exports.get_function("process_bytes").unwrap();
 
-    let byte_array: Vec<u8> = vec![65, 66, 67, 68, 69];
-    let byte_array_len = byte_array.len() as i32;
+    // Example byte array
+    let byte_array: Vec<u8> = vec![0x48, 0x65, 0x6C, 0x6C, 0x6F];
 
+    // Call the exported function with the byte array
     let result = function.call(&mut store, &[
-        wasmer::Value::I32(byte_array.as_ptr() as i32),
-        byte_array_len.into()
+        Value::I32(byte_array.as_ptr() as i32),
+        Value::I32(byte_array.len() as i32)
     ]).unwrap();
 
-    println!("result {:?}", result);
+    // Check the result
+    println!("Result: {:?}", result);
 
-    // // Get transactions
-    // let txs =  get_tx_data();
-
-    // for tx in &txs {
-    //     let tx_serialized = to_vec(tx).unwrap();
-    //     println!("length {:?}", tx_serialized.len());
-
-    //     // let decoded_data = TxData::try_from_slice(tx_serialized.as_slice()).unwrap();
-    //     // println!("decoded data {:?}", decoded_data);
-
-    //     let result = function.call(&mut store, &[
-    //         wasmer::Value::I32(tx_serialized.as_ptr() as i32),
-    //         wasmer::Value::I32(tx_serialized.len() as i32)
-    //     ]).unwrap();
-    //     println!("result {:?}", result);
-
-    //     // assert_eq!(result[0], Value::I32(7));
-    // }
-
-
-}
-
-fn print_number_to_console(num: i32) {
-    println!("Number received from WebAssembly: {}", num);
 }
